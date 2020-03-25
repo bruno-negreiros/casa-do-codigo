78a6699e84182cc26b58d9a61d15318c91f980bf
Bruno Negreiros <brunoananias_n@hotmail.com>
2020-03-25 18:56:02 -0400

    Organização do projeto

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const http = require('http');
 
 const servidor = http.createServer(function (req, resp) {
-    resp.end(`
+
+    let html;
+
+    if (req.url == '/') {
+        html = `
         <html>
             <head>
                 <meta charset="utf-8">
@@ -10,6 +14,21 @@ const servidor = http.createServer(function (req, resp) {
                 <h1>Casa do Código</h1>
             </body>
         </html>
-    `);
+        `;
+    } else if (req.url == '/livros') {
+        html = `
+        <html>
+            <head>
+                <meta charset="utf-8">
+            </head>
+            <body>
+                <h1>pinto</h1>
+            </body>
+        </html>
+        `;
+    }
+
+    resp.end(html);
+              
 });
 servidor.listen(3000); // porta padrão dos servidores node
