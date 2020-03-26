e017519d225aed4eb458732b213fa24cb1ebd70a
Bruno Ananias Negreiros <brunoananias_n@hotmail.com>
2020-03-25 21:41:19 -0400

    Encapsulando as rotas

---
+++ src/app/rotas/rotas.js
@@ -0,0 +1,37 @@
+module.exports = (app) => {
+    // Criando as rotas
+    app.get('/', function(req, resp) {
+        resp.send(`
+        <html>
+            <head>
+                <meta charset="utf-8">
+            </head>
+            <body>
+                <h1>Casa do Código</h1>
+            </body>
+        </html>
+        `);
+    });
+
+    app.get('/livros', function(req, resp) {
+        resp.send(`
+        <html>
+            <head>
+                <meta charset="utf-8">
+            </head>
+            <body>
+                <h1>Lista de livros:</h1>
+                <ul>
+                    <li>Java</li>
+                    <li>Angular</li>
+                    <li>Node</li>
+                    <li>React</li>
+                </ul>
+            </body>
+        </html>
+        `);
+    });
+}
+
+
+
--- src/config/custom-express.js
+++ src/config/custom-express.js
@@ -1,4 +1,7 @@
 const express = require('express'); // retorna uma função
 const app = express();
 
+const rotas = require('../app/rotas/rotas');
+rotas(app);
+
 module.exports = app;
